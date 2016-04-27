e74155c2dd24398c2bd8b987d705d0630ef1dc0c
Justin Holmes <justin@nascency.co.uk>
2016-04-27 14:20:39 +0200

    24 - added local cache of the response from the api

--- app/resources/js/main.js
+++ app/resources/js/main.js
@@ -3,6 +3,8 @@ window.$ = window.jQuery = require('./bower_components/jquery/dist/jquery.min.js
 var EnabledServicesAPI = require("./resources/js/services/EnabledServicesAPI.js");
 
 EnabledServicesAPI.getServices();
+// should be from local app cache.
+EnabledServicesAPI.getServices();
 
 // This is a mock function, nothing like something we really should use, just for testing purposes
 var switchWebview = function(webview){
--- app/resources/js/services/EnabledServicesAPI.js
+++ app/resources/js/services/EnabledServicesAPI.js
@@ -1,15 +1,22 @@
 var request = require('request');
 
+var data = null;
+
 EnabledServicesAPI = {
 
     getServices: function() {
-        return request('http://localhost:8080/api/services/', function (error, response, body) {
-          if (!error && response.statusCode == 200) {
-            return body;
-          } else {
-            return null;
-          }
-        });
+        if (data === null) {
+            return request('http://localhost:8080/api/services/', function (error, response, body) {
+              if (!error && response.statusCode == 200) {
+                data = body;
+                return body;
+              } else {
+                return null;
+              }
+            });
+        } else {
+            return data;
+        }
 
     }
 }
