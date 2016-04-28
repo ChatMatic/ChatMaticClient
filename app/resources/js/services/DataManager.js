2586d7f1ec3a2b31f58a402f05a0f2eb3cb9aab8
Phil Hudson <philhudson91@gmail.com>
2016-04-28 10:47:29 +0700

    tidied up code, locally storing enabled services. getting enabled services on launch. improved error handling

--- app/resources/js/services/DataManager.js
+++ app/resources/js/services/DataManager.js
@@ -4,67 +4,93 @@
 const storage = require('electron-json-storage');
 
 StorageKeys = {
-    UserAccounts : "user_accounts"
+    UserAccounts: "user_accounts",
+    EnabledServices: "enabled_services"
 };
 
-function count(obj) { return Object.keys(obj).length; }
+function count(obj) {
+    return Object.keys(obj).length;
+}
 
 
 DataManager = {
 
-  AddNewAccount: function(data, callback) {
+    AddNewAccount: function (data, callback) {
 
-      var newStorageObj = {
-          0 : data
-      };
+        var newStorageObj = {
+            0: data
+        };
 
-      this.GetExistingAccounts(function(error, existingAccounts) {
+        this.GetExistingAccounts(function (error, existingAccounts) {
 
-          if (!error) {
+            if (!error) {
 
-              if (existingAccounts != null || existingAccounts != undefined) {
+                if (existingAccounts != null || existingAccounts != undefined) {
 
-                  var countOfProperties = count(existingAccounts);
+                    var countOfProperties = count(existingAccounts);
 
-                  if (countOfProperties >= 0) {
+                    if (countOfProperties >= 0) {
 
-                      countOfProperties += 1;
-                      existingAccounts[countOfProperties] = data;
+                        countOfProperties += 1;
+                        existingAccounts[countOfProperties] = data;
 
-                  } else {
+                    } else {
 
-                      existingAccounts = newStorageObj
-                  }
+                        existingAccounts = newStorageObj
+                    }
 
-              } else {
+                } else {
 
-                  existingAccounts = newStorageObj
-              }
+                    existingAccounts = newStorageObj
+                }
 
-              storage.set(StorageKeys.UserAccounts, existingAccounts, function (error) {
-                  if (error) {
-                       return callback(error, null);
-                  } else {
-                      console.log("data save success with payload: " +  existingAccounts    );
+                storage.set(StorageKeys.UserAccounts, existingAccounts, function (error) {
+                    if (error) {
+                        return callback(error, null);
+                    } else {
+                        console.log("data save success with payload: " + existingAccounts);
 
-                      return callback(null, true);
-                  }
-              });
-          } else {
-              console.log(error);
-          }
+                        return callback(null, true);
+                    }
+                });
+            } else {
+                console.log(error);
+            }
 
-      });
+        });
 
-  },
+    },
 
     GetExistingAccounts: function (callback) {
 
-        storage.get(StorageKeys.UserAccounts, function(error, data){
+        storage.get(StorageKeys.UserAccounts, function (error, data) {
             if (error) {
                 return callback(error, null);
             } else {
                 console.log('existing accounts ' + JSON.stringify(data));
+                return callback(null, data);
+            }
+        });
+    },
+
+    SaveEnabledServices: function(data, callback) {
+        storage.set(StorageKeys.EnabledServices, data, function (error) {
+            if (error) {
+                return callback(error, null);
+            } else {
+                console.log("data save success with payload: " + data);
+
+                return callback(null, true);
+            }
+        });
+    },
+
+    GetEnabledServices: function(callback) {
+        storage.get(StorageKeys.EnabledServices, function (error, data) {
+            if (error) {
+                return callback(error, null);
+            } else {
+                console.log('enabled services ' + JSON.stringify(data));
                 return callback(null, data);
             }
         });
